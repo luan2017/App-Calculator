f5e89aa2cb8f64ef33af41901eae3e09892f3233
Luan Pinho <luanpinhoo@gmail.com>
2019-05-10 13:18:13 -0400

    configurando functions dos buttons_1

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
     
 //importando class dos Botões
 import InputNumberButton from './InputNumberButton' 
 
 //criando as const dos meus botões
 const buttons = [ 
-  [ 'CLEAR', 'DEL'],
-  [7, 8, 9, '/'],
-  [4, 5, 6, 'x'],
-  [1, 2, 3, '-'],
-  [0, '.', '=', '+']
+  [ 'Clear', 'DL', 'H'],             //CLEAR = zerar, DEL: apagar    H: Histórico
+  ['7', '8', '9', '/'],
+  ['4', '5', '6', 'x'],
+  ['1', '2', '3', '-'], 
+  ['0', '.', '=', '+']
 ];
 
 export default class App extends Component {
 
+  constructor() {
+    super()
+    this.initialState = {
+      displayValue: '0',
+      operator : null                     //estado do operador operador
+    }
+    this.state  = this.initialState; 
+ }
+
   //criando um render para os meus botões
   renderButtons() {
-    let layouts = buttons.map((buttonRows, index) => {
-      let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+    let layouts = buttons.map((buttonRows, index) => {      //Matriz dos Botões que vai retornar o valor no container
+      let rowItem = buttonRows.map((buttonItems, buttonIndex) => {  
         return <InputNumberButton
+          value={buttonItems}                               //atribuindo valor aos meus botões
+          handleOnPress={this.handleInput.bind(this, buttonItems)}       // {() => {} }  retorna vazio //
           key={'btn-' + buttonIndex} />
       });
       return <View style={styles.inputRow} key={'row-' + index}>{rowItem}</View>
@@ -28,15 +38,65 @@ export default class App extends Component {
     return layouts
   }
 
-  render() {
+  handleInput = (input) => {                            //definindo um valor inicial a minha calculadora
+    const {displayValue, operator} = this.state;
+    switch (input) {  //numeros de 0 a 9
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7': 
+        case '8':
+        case '9':
+          this.setState({
+            displayValue: (displayValue === '0') ? input : displayValue + input     // se o valor de entrada for =! 0 o valor substitui
+          })    
+          break;  //operadores lógicos
+        case '+':
+        case '-':
+        case 'x':
+        case '/':
+          this.setState({
+            operator: input,
+            displayValue : (operator !== null ? displayValue.substr(0, displayValue.length -1) : displayValue) + input 
+                              // condição para não deixar repitir os operadores na mesma sequiencia
+          })  
+          break;
+        case '.':
+          let dot = displayValue.slice(-1)          //  pega o ultimo caractere
+          this.setState({
+            displayValue: dot !== '.' ? displayValue + input : displayValue       // se o ultimo digito for '.' ele não aceita mais o mesmo ponto
+          })
+          break;
+        case 'Clear':
+          this.setState(this.initialState);
+          break;
+        case 'DL':
+          let string = displayValue.toString();
+          let deletedString = string.substr(0, string.length -1)
+          let length = string.length
+          this.setState  ({
+            displayValue: length == 1 ? '0' : deletedString
+
+          })
+    }
+    
+  }
+  
+  render() { 
     return (
       <View style={styles.container}>
           <View style={styles.resultContainer}>
-            <Text style={styles.resultText}>0</Text>
+            <Text style={styles.resultText}>
+                {this.state.displayValue}
+            </Text>
           </View>
 
-          <View style={styles.inputContainer}>
-
+          <View style={styles.inputContainer}> 
+            {this.renderButtons()}
           </View>
       </View>
     );
@@ -50,7 +110,7 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     flex: 2,
-    //justifyContent: 'center',
+    justifyContent: 'center',    // centraliza meus numeros mesmo ao girar a tela
     backgroundColor: '#4169E1'  // container secundario Azul Claro
   },
   inputContainer: {
@@ -63,6 +123,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',         // linha em Negrito
     padding: 20,                // Altura do meu Texto
     textAlign: 'right'          //lado que meu numero vai ficar
+  },
+  inputRow: {
+    flex: 1,
+    flexDirection: 'row'        //Organiza os quadrados dos botões
   }
 
 });
--- inputNumberButton.js
+++ inputNumberButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react';       //importações
 
 import { StyleSheet, Text, View, TouchableOpacity  } from 'react-native';
 
@@ -6,29 +6,27 @@ import { StyleSheet, Text, View, TouchableOpacity  } from 'react-native';
 export default class InputNumberButton extends Component {
   render() {
     const {value, handleOnPress} = this.props;
-
     return (
-        <TouchableOpacity 
-          style={styles.container}
-          onPress={()=> handleOnPress(value)} >
-
-        <Text style={styles.text}>{value}</Text>        
+        <TouchableOpacity style={styles.container} onPress={() => handleOnPress(value)} >     
+          <Text style={styles.text}>{value}</Text>    
         </TouchableOpacity>
     );
   }
 }
 
-const styles = Stylesheet.create({
+const styles = StyleSheet.create({
   container: {
-    fkex 1,
+    flex: 1,
     margin: 1,
-    backgroundColor: 'rbga(255, 255, 255, 0.1)',
-    justfyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    justifyContent: 'center',
     alignItems: 'center'
   },
   text: {
-    color: 'white,',
-    fontSize: 26
-  }
+    color: 'white',                  // Cor dos botões
+    fontSize: 40,                   // Tamanho da Fonte dos meus botões 
+    fontWeight: 'bold'              // Deixando meus botões em Negrito
 
+  }
+ 
 });
